481c78aad47270acf381d45ecd7c42bf902b0d37
Shreedhar29 <Shreedharvss@gmail.com>
2023-02-10 16:09:58 +0530

    Modified

--- second.c
+++ second.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
 
-#define pi 3.14f /* Define the value of pie */
+#define pi 20
+int main()
+{
+	int n,sum;
+	printf("ENter a n value\n");
+	scanf("%d",&n);
+        sum=pi*n;
+	printf("%d\n",sum);
 
-void main() {
-  /* Variable Declaration. */
-  float radius, perimeter, area;
-
-  /* Taking input of the radious of the circle from the user */
-  printf("Enter radius of the Circle:\n");
-  scanf("%f", & radius);
-
-  /* Calculating perimeter of the circle */
-  perimeter = 2 * pi * radius;
-  printf("Perimeter of the circle: %0.4f\n", perimeter);
-
-  /* Calculating area of the circle */
-  area = pi * radius * radius;
-  printf("Area of circle: %0.4f\n", area);
 }
+
