39d679c06ad78470a58606d691ca341d85c674c4
Shreedhar29 <Shreedharvss@gmail.com>
2023-02-13 17:23:14 +0530

    codeing1

---
+++ systemclassprograms/prog4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+   int num,rem=0,rev=0,sum=0,product=1;
+   printf("enter a number\n");
+   scanf("%d",&num);
+   while(num!=0)
+   {
+   rem=num%10;
+   sum=sum+rem;
+   product = product*rem;
+   rev =(rev *10)+rem;
+   num=num/10;
+   }
+
+   printf("sum: %d\n",sum);
+   printf("product:%d\n",product);
+
+}
